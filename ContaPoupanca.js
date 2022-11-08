b88b8e88a45bf7b526088e5bf32434d12f74c7b9
N0TRIA <airtonlameraaliivl@gmail.com>
2022-11-08 16:00:13 -0300

    Trabalhando com herança
    Especializando código
    
    Herdando propriedades e atributos
    
    Sobrecarga de métodos
    
    Compartilhando código de forma segura
    
    Mantendo nosso código na linguagem do cliente
    
    Evitando código muito complicado através da herança
    
    Palavra-chave Super
    
    Método e propriedades privadas e protegidas

--- ContaPoupanca.js
+++ ContaPoupanca.js
@@ -1,8 +1,8 @@
-export class ContaPoupanca{
+import { Conta } from './Conta.js'
+
+export class ContaPoupanca extends Conta{
     constructor(saldoInicial, cliente, agencia) {
-        this._saldo = saldoInicial;
-        this._cliente = cliente;
-        this._agencia = agencia;
+        super(saldoInicial, cliente, agencia);
     }
 
     sacar(valor){
@@ -10,20 +10,5 @@ export class ContaPoupanca{
             this._saldo -= valor;
             return valor;
         }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
-    }
-
-    tranferir(valor, conta){
-        
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-        
     }
 }
