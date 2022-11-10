edbc796558d2413dfd1dd4ce4570d0f6eff2ea68
N0TRIA <airtonlameraaliivl@gmail.com>
2022-11-10 14:36:00 -0300

    Aplicando Interfaces e Duck Type
    -operador in
    -operador instanceOf
    -Como proteger nossos sistemas
    -interfaces padronizadas
    -Ducky Type

--- Funcionario/SistemaAutenticacao.js
+++ Funcionario/SistemaAutenticacao.js
@@ -2,8 +2,16 @@
 
 import { Funcionario } from "./Funcionario.js";
 
-export class SistemaAutenticacao extends Funcionario{
-    static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+export class SistemaAutenticacao extends Funcionario {
+    static login(autenticavel, senha) {
+        if (SistemaAutenticacao.ehautenticavel(autenticavel)) {
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehautenticavel(autenticavel) {
+        return 'autenticar' in autenticavel &&
+        autenticavel.autenticar instanceof Function
     }
 }
